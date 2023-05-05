f6f6dfd1ba288793e201d0bb67b3a6176d743626
Kenneth Carlson <carlson90@hotmail.com>
2023-05-05 10:22:34 +0200

    log in js slider?

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,8 @@
-window.onload = function () {
-    const form = document.getElementById("login");
-    form.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      try {
-        const response = await fetch("https://api.noroff.dev/api/v1/auction/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: email, password }),
-        });
-  
-        if (response.ok) {
-          const { jwt } = await response.json();
-          localStorage.setItem("jwt", jwt);
-          window.open("https://www.google.com", "_blank");
-        } else {
-          throw new Error("login failed");
-        }
-      } catch (error) {
-        console.error(error);
-        alert("dude wrong password!");
-      }
-    });
-  };
-  
+const images = document.querySelectorAll(".slider img");
+let currentImage = 0;
+
+setInterval(() => {
+  images[currentImage].style.opacity = 0;
+  currentImage = (currentImage + 1) % images.length;
+  images[currentImage].style.opacity = 1;
+}, 3000);
