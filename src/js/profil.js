cc4271620813e41d7979191bc6345285b720569d
Kenneth Carlson <carlson90@hotmail.com>
2023-05-30 16:05:53 +0200

    add more stuff

--- src/js/profil.js
+++ src/js/profil.js
@@ -1,8 +1,24 @@
-const images = document.querySelectorAll(".slider img");
-let currentImage = 0;
+const token = localStorage.getItem("jwt");
 
-setInterval(() => {
-  images[currentImage].style.opacity = 0;
-  currentImage = (currentImage + 1) % images.length;
-  images[currentImage].style.opacity = 1;
-}, 3000);
+if (token) {
+  // const personName = 'yellow_pig';
+  const apiUrl = "https://api.noroff.dev/api/v1/auction/profiles";
+  // const apiUrl = `https://api.noroff.dev/api/v1/auction/profiles/${personName}`;
+
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //data
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("En feil oppstod ved henting av data:", error);
+    });
+} else {
+  console.error("JWT-tokenet er ikke tilgjengelig i localStorage.");
+}
